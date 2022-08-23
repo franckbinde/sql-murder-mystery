fd3ba02242367b520c09c89224284c497b435ecb
Franck Binde <franckbinde@yahoo.fr>
2022-08-24 00:09:13 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SQL Code For The SQL Murder Mystery
+
+## Introduction
+There's been a Murder in SQL City! The SQL Murder Mystery is designed to be both a self-directed lesson to learn SQL concepts and commands and a fun game for experienced SQL users to solve an intriguing crime.
+
+## Source
+You can try this challenge yourself [here](https://mystery.knightlab.com/)
+
+## Who is the murderer?
+The code in this repository is one of the possible solution codes to solve the challenge.
---
+++ code.sql
@@ -0,0 +1,17 @@
+-- sqlite
+SELECT g.membership_id, m.person_id, m.name, p.license_id
+	FROM get_fit_now_check_in g 
+	INNER JOIN get_fit_now_member m
+	ON g.membership_id = m.id
+	INNER JOIN person p
+	ON p.id = m.person_id
+	WHERE g.check_in_date = 20180109 AND g.membership_id IN (
+	  SELECT id
+			FROM get_fit_now_member 
+			WHERE membership_status = "gold" and id LIKE '48Z%' 
+	  )
+	  AND p.license_id IN (
+		SELECT id 
+			FROM drivers_license 
+			WHERE plate_number LIKE '%H42W%' AND gender = 'male'
+		)
